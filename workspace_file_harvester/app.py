382e5614145fe4251bc32daaa6887157a45f3c1c
hcollingwood <hannah.collingwood@telespazio.com>
2025-02-03 15:58:45 +0000

    EODHP-1117 - linting

--- workspace_file_harvester/app.py
+++ workspace_file_harvester/app.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 
-import click
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
@@ -13,16 +12,6 @@ app = FastAPI(root_path=root_path)
 s3_bucket = os.environ.get("S3_BUCKET")
 
 
-@click.group()
-# you can implement any global flags here that will apply to all commands, e.g. debug
-# @click.option('--debug/--no-debug', default=False) # don't feel the need to implement this, just an example
-def cli():
-    """This is just a placeholder to act as the entrypoint, you can do things with global options here
-    if required"""
-    pass
-
-
-@cli.command()
 @app.post("/{workspace_name}/harvest")
 async def root(workspace_name: str):
 
